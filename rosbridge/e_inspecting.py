f237a6608888f58b214768cd43096ebdfb682312
klee <hihahu@posteo.de>
2023-01-24 15:44:47 +0100

    new interface for inspecting example

--- rosbridge/e_inspecting.py
+++ rosbridge/e_inspecting.py
@@ -2,11 +2,12 @@ import json
 
 from mir import MirInspect
 
-sdt_host = "mir.com"
+# Set Host
+sdt_host = "192.168.12.20"
 
-print(f"Enter mir's network-address: (hit enter to use '{sdt_host}')")
+print(f"Enter mir's network-address (hit enter to use '{sdt_host}'): ", end="")
 usr_host = input()
-print("OK, Trying to connect to MiR...")
+print("Trying to connect to MiR...")
 
 # Connecting to MiR
 host = usr_host if len(usr_host) > 0 else sdt_host
@@ -17,16 +18,40 @@ except:
   exit()
 print("Connected!\n")
 
-# Inspecting
-print("Getting all available topics:")
-l = mir.topic_list()
-print(json.dumps(l, indent=3))
+# Command Mode
 
-print()
-print("Give a topic to echo (Press any key to aboard): ")
-t = None
-while t not in l: t = input()
-t = mir.topic_start_echo(t)
-input()
-mir.topic_stop_echo(t)
-mir.terminate()
+c = None
+
+while True:
+  print("\nAvailable commands:")
+  print("l - list all topics")
+  print("e - echo specific topic")
+  print("x - terminate and exit")
+  print("> ", end="")
+  c = input()
+  print()
+
+  match c[0]:
+
+    case "l":
+      print("Getting all topics...")
+      l = mir.topic_list()
+      l.sort()
+      for i in l: print(i)
+
+    case "e":
+      t_name = c[2:] # Get topic name from command
+      if len(c) <= 1:
+        print("Give a topic to echo: ", end="")
+        t_name = input()
+      t = mir.topic_start_echo(t_name)
+      print("Started echoing, press enter to stop ...")
+      input()
+      mir.topic_stop_echo(t)
+    
+    case "x":
+      mir.terminate()
+      exit()
+
+    case _:
+      print("Unknown command!")
